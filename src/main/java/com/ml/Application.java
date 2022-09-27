ac1a8f0707449d15673a6dd85bd6d633f97707ae
Timothy Lam Casem <timothylam96@hotmail.com>
2022-09-28 02:01:34 +0800

    add migration

--- src/main/java/com/ml/Application.java
+++ src/main/java/com/ml/Application.java
@@ -1,8 +1,14 @@
 package com.ml;
 
 import io.micronaut.runtime.Micronaut;
+import io.micronaut.serde.annotation.SerdeImport;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import liquibase.ContextExpression;
+import liquibase.Labels;
+import liquibase.change.CheckSum;
+import liquibase.changelog.RanChangeSet;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 @OpenAPIDefinition(
 		info = @Info(
@@ -11,8 +17,16 @@ import io.swagger.v3.oas.annotations.info.Info;
 		)
 )
 
+@SerdeImport(RanChangeSet.class)
+@SerdeImport(CheckSum.class)
+@SerdeImport(ContextExpression.class)
+@SerdeImport(Labels.class)
 public class Application {
 	public static void main(String[] args) {
+		// Bridge JUL to Slf4j
+		SLF4JBridgeHandler.removeHandlersForRootLogger();
+		SLF4JBridgeHandler.install();
+
 		Micronaut.run(Application.class, args);
 	}
 }
